d68efe814f23b7bb9560ae9818f947ba9308f78f
dibborah <dibyajyotiborah469@gmail.com>
2024-02-24 21:56:32 +0530

    Mostly Repracticed along with some comments for understanding. Form creation is completed along with handleChangeInput and handleSubmit.

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,27 +3,44 @@ import { useState } from "react";
 const PostForm = () => {
   const [post, setPost] = useState({
     title: "",
-    body: ""
+    body: "",
   });
-
-  console.log(post);
 
   const handleChangeInput = (e) => {
     setPost({
-        ...post,
-       [ e.target.name] : e.target.value
-    })
-  }
+      ...post,
+      // Dynamically the keys are created and set
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const renderField = (label) => (
     <div>
       <label>{label}</label>
-      <input onChange={handleChangeInput} type="text" name={label.toLowerCase()} value={post[label.toLowerCase()]}/>
+      {/* The name attribute is set dynamically according to what arguement would be passed as input in the parameter label */}
+      <input
+        onChange={handleChangeInput}
+        type="text"
+        name={label.toLowerCase()}
+        value={post[label.toLowerCase()]}
+      />
     </div>
   );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(post);
+    setPost({
+      title: "",
+      body: "",
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       {renderField("Title")}
       {renderField("Body")}
+      <button type="submit">Submit</button>
     </form>
   );
 };
