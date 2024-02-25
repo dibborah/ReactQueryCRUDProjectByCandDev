dacd47693122c8f28a59bbe71dd4bb7398604508
dibborah <dibyajyotiborah469@gmail.com>
2024-02-25 21:53:08 +0530

    data added in db.json server and We are able to FETCH(GET) data from the server and able to populate the data in the screen

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,10 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
 import AddPost from "../components/AddPost";
+import { fetchPosts } from "../api/posts";
+import { useNavigate } from "react-router-dom";
 
 const PostList = () => {
+  const navigate = useNavigate();
+  const {
+    isLoading,
+    isError,
+    data: posts,
+    error,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) return "...Loading";
+  if (isError) return `Error: ${error.message}`;
+
   return (
     <div>
-      <AddPost/>
-      Lists
+      <AddPost />
+      {posts &&
+        posts.map((post) => (
+          <div key={post.id} style={{ background: "#777" }}>
+            <h4 style={{cursor: "pointer"}} onClick={() => navigate(`/post/${post.id}`)}>{post.title}</h4>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        ))}
     </div>
   );
 };
