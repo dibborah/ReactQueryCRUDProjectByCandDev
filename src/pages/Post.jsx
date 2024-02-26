d4e89dcf87569c194ec8eebcfb456a38e7b622bc
dibborah <dibyajyotiborah469@gmail.com>
2024-02-26 20:57:39 +0530

    Able to fetch and and populate individual posts. Also added devtools in the mian.js (Application)

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,7 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import { fetchPost } from "../api/posts";
+
 const Post = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const {
+    isLoading,
+    isError,
+    data: post,
+    error,
+  } = useQuery({
+    queryKey: ["posts", id],
+    queryFn: () => fetchPost(id),
+  });
+
+  if (isLoading) return "...Loading";
+  if (isError) return `Error: ${error.message}`;
+
   return (
     <div>
-      <h1>Post</h1>
+      <button onClick={() => navigate("/")}>Back</button>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 };
